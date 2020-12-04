e2662f0730fc8cc8deb9123819d36cbe7f41358b
okidijimmy200 <okidijimmie@gmail.com>
2020-12-04 11:34:10 +0300

    Implementing the GameForm component

--- client/game/GameForm.js
+++ client/game/GameForm.js
@@ -63,21 +63,36 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-/* */
+/* The GameForm component is used in both the NewGame and EditGame components,
+and it contains the elements that allow users to enter game details and VR object
+details for a single game*/
+////////////////////////////////////////////////////////////////
+/*It may start with a blank game object or load an existing
+game. To begin the implementation of this component, we will first initialize a blank
+game object in the component state, */
 export default function GameForm(props) {
   const classes = useStyles()
   const [readError, setReadError] = useState('')
+  /*To begin the implementation of this component, we will first initialize a blank
+game object in the component state */
   const [game, setGame] = useState({name: '', clue:'', world:'', answerObjects:[], wrongObjects:[]})
 
+  /*If the GameForm component receives a gameId prop from the parent component—such as from the EditGame component—then it will use the load game
+API to retrieve the game's details and set it to the state, to be rendered in the form view. */
   useEffect(() => {
+    /*In the userEffect hook, we first check if the props received from the parent
+component contain a gameId prop, and then use the value to make the load game
+API call */
     if(props.gameId){
       const abortController = new AbortController()
       const signal = abortController.signal
     
       read({gameId: props.gameId}, signal).then((data) => {
         if (data.error) {
+          /*If the API call returns an error, we set the error to the state; */
           setReadError(data.error)
         } else {
+          /*we set the retrieved game to the state. */
           setGame(data)
         }
       })
@@ -87,7 +102,12 @@ export default function GameForm(props) {
     }
   }, [])
 
+  /*This handleChange method will update the game values in the state whenever a user
+changes a value in an input element */
   const handleChange = name => event => {
+    /*based on the specific field value being changed, we update the
+corresponding attribute in the game object in the state. This captures the values
+entered by the user as simple details for their VR game */
     const newGame = {...game}
     newGame[name] = event.target.value
     setGame(newGame)
@@ -109,15 +129,26 @@ export default function GameForm(props) {
     setGame(newGame)
   }
     return (
+      /*The form view part in the GameForm component will essentially have two parts: one
+part that takes simple game details—such as name, world image link, and clue
+text—as input, and a second part that allows users to add a variable number of VR
+objects to either the answer objects array or the wrong objects array. */
       <Card className={classes.card}>
         <CardContent>
           <Typography type="headline" component="h2" className={classes.title}>
+            {/* Form title: The form title will be either New Game or Edit Game, depending on whether an existing game ID is passed as a prop to
+GameForm from the parent component to which it is added, */}
             {props.gameId? 'Edit': 'New'} Game
           </Typography>
+          {/* Game world image input: We will render the background image URL in an img element at the very top of the form to show users the image they
+added as the game world image URL. The image URL input will be taken in a TextField component below the rendered image */}
           <img src={game.world} className={classes.imgPreview}/>
           <TextField id="world" label="Game World Equirectangular Image (URL)" className={classes.textField} value={game.world} onChange={handleChange('world')} margin="normal"/><br/>
+          {/* Game name: The game name will be added in a single TextField of the default text type */}
           <TextField id="name" label="Name" className={classes.textField} value={game.name} onChange={handleChange('name')} margin="normal"/><br/>
+          {/* Clue text: The clue text will be added to a multiline TextField component, */}
           <TextField id="multiline-flexible" label="Clue Text" multiline rows="2" value={game.clue} onChange={handleChange('clue')} className={classes.textField} margin="normal"/><br/>
+          {/* In these form elements added to the GameForm component, the input fields also take an onChange handler function, which is defined as handleChange */}
           <Divider className={classes.spacingTop}/>
           <ExpansionPanel>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
